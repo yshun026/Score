2c3be8a7655ab957b073ce4d0e42a2c2b45be4f8
yan <scott317438@gmail.com>
2026-05-11 10:44:55 +0800

    Add Max and Min

--- Score.py
+++ Score.py
@@ -1,8 +1,16 @@
 scores = input().split()
 
 count = 0
+maxn = int(scores[0])
+minn = int(scores[0])
+
 for s in scores:
     if int(s) < 60:
         count += 1
+    if int(s) > maxn:
+        maxn = int(s)
+    if int(s) < minn:
+        minn = int(s)
 
 print(count)
+print(f"max: {maxn}, minn: {minn}")
